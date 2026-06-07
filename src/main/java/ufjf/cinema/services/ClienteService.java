8e1187c8ae89ea9e0823e6d07a721e212f6678c2
isaac-kapela <kapelajoao4@gmail.com>
2026-06-07 20:46:21 -0300

    cria servico clientte  e controller

---
+++ src/main/java/ufjf/cinema/services/ClienteService.java
@@ -0,0 +1,24 @@
+package ufjf.cinema.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import ufjf.cinema.model.entity.Cliente;
+import ufjf.cinema.model.repository.ClienteRepository;
+
+@Service
+public class ClienteService extends CrudServiceBase<Cliente, Long> {
+    private ClienteRepository clienteRepository;
+
+    public ClienteService(JpaRepository<Cliente, Long> repository) {
+        super(repository);
+        this.clienteRepository = (ClienteRepository) repository;
+    }
+
+    @Override
+    public void validar(Cliente cliente) {
+        validarCampo(cliente.getNome(), "nome");
+        validarCampo(cliente.getEmail(), "email");
+        validarCampo(cliente.getSenha(), "senha");
+        validarCampo(cliente.getTelefone(), "telefone");
+    }
+}
